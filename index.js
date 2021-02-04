9b69522e864642956ef4b7464902b02d3bf471f5
Victoria <berling2818@gmail.com>
2021-02-04 23:50:51 +0300

    changed

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-let array = [];
 const submit = document.querySelector("#submit");
 const firstValue = document.querySelector("#firstValue");
 const lastValue = document.querySelector("#lastValue");
@@ -8,27 +7,27 @@ let result = "";
 submit.addEventListener("click", (event) => {
   event.preventDefault();
 
-  array.push({ firstValue: firstValue.value, lastValue: lastValue.value });
-  result = (lastValue.value - firstValue.value) / 100;
+  result = (+lastValue.value - +firstValue.value) / 100;
 
-  console.log(array);
   console.log(result);
-
-  drewBox();
   form.reset();
+  drewBox();
 });
 
 const drewBox = () => {
   const parent = document.querySelector("#parent");
 
-  let childWidth = result * parent.offsetWidth;
+  let childWidth = result * +parent.offsetWidth;
 
-  parent.innerHTML = "";
+  parent.innerHTML += ``;
 
-  parent.innerHTML += `<div class = "child" style="width:${childWidth}px; height:100px; background-color: blueviolet;position: absolute; margin-left:${childWidth}px"></div> `;
+  parent.innerHTML += `
+  <div class = "child" style="width:${childWidth}px; height:100px; background-color: blueviolet; margin-left:${childWidth}px"></div>
+   `;
+
   const child = document.querySelector(".child");
+
   console.log(parent.offsetWidth);
   console.log(childWidth);
-
   console.log(child.offsetLeft);
 };
